293d7925b515acbb6cb562c2af6d3126569c2036
mei654654 <91170742+mei654654@users.noreply.github.com>
2021-09-29 12:05:51 +0800

    Add files via upload

---
+++ week03-4.cpp
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf( "Hallo World\n" );
+    printf( "%d" , 100+200 );
+}
---
+++ week03-5.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    printf( "Hallo World\n" );
+    printf( "Hallo%dWorld\n" , 100+200 );
+    printf( "%d %d    %d" , 300, 200, 100);
+}
+
