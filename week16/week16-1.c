b293fd718d3d5a582bd8d2d407175ef2eff2b640
mei654654 <91170742+mei654654@users.noreply.github.com>
2021-12-29 11:37:43 +0800

    Add files via upload

---
+++ week16/week16-1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int a[100];
+char s[20];
+int main()
+{
+    printf("請輸入你的名子: ");
+    scanf("%s",s);
+    ///Q: scanf("%d",&n);
+
+    printf("你剛剛輸入:%s\n",s);
+}
---
+++ week16/week16-2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+char line[20];
+int main()
+{
+    printf("Please input line: ");
+    scanf("%s",line);
+    ///Q: scanf("%d",&n);
+
+    printf("%s\n",line);
+}
