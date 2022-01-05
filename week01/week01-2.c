4f266431d8c3a01bca83b74ca98114375b26b891
mei654654 <91170742+mei654654@users.noreply.github.com>
2022-01-05 11:31:21 +0800

    Add files via upload

---
+++ week01/week01-2.c
@@ -0,0 +1,13 @@
+// 兩個斜線 slash
+///Q3: 三個斜線 slash 可以寫中文的解釋,方便理解
+/// (3)寫完程式,按上方的 Build&Run (齒輪+綠三角)
+/// (1) 每次開一個新的 Empty File
+///   File-New-Empty File (Ctrl-Shift-N)
+/// (2) 存檔案 File-SaveAs 另存新檔
+///   1016xxxx-1.cpp 不要有空格,都用英數符號
+       ///小心,不要拼錯,不是 studio
+#include <stdio.h>  /// #(大寫3) <>(大寫逗號/句號)
+int main() ///圓括號在大寫9,0
+{                  ///反斜線 backslash
+    printf( "Hello World\n" );
+}/// 有個f  雙引號在大寫的 Enter旁
