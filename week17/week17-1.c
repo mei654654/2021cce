8a52bc3bd23061d146731baa456ea38aebc4d530
mei654654 <91170742+mei654654@users.noreply.github.com>
2022-01-05 11:27:04 +0800

    Add files via upload

---
+++ week17/week17-1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int N=20031010;
+    printf("%d=> %d %d\n", N, N/10, N%10);
+
+    N=N/10;
+    printf("%d=> %d %d\n", N, N/10, N%10);
+
+    N=N/10;
+    printf("%d=> %d %d\n", N, N/10, N%10);
+    N=N/10;
+    printf("%d=> %d %d\n", N, N/10, N%10);
+    N=N/10;
+    printf("%d=> %d %d\n", N, N/10, N%10);
+
+}
