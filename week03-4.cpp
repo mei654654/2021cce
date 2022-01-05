c98f771c590ca42b8a226b0a5ccca426e3eebf34
mei654654 <10160872@me.mcu.edu.tw>
2022-01-05 11:58:35 +0800

    week03

--- week03-4.cpp
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    printf( "Hallo World\n" );
-    printf( "%d" , 100+200 );
-}
--- week03-5.cpp
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    printf( "Hallo World\n" );
-    printf( "Hallo%dWorld\n" , 100+200 );
-    printf( "%d %d    %d" , 300, 200, 100);
-}
-
